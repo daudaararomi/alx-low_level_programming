4e559c0fb590829edfac54d97f2064d035afa767
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-08 15:28:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-/* first orogramm */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-/* use of puts()*/
-{puts("\"Programming is like building a multilingual puzzle\n");
-return (0); }
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
