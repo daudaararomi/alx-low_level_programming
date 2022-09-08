ce06188127d48e5cc00c75f54ceb3d391c97df97
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-08 16:08:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, s, strlen(s));
 	return (1);
