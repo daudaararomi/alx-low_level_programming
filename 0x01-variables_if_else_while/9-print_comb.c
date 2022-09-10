d82596d15890477d7e9fe7ee1375ec3cbafbb180
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-09 18:55:58 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c);
+		putchar(',');
+		putchar(' ');
+	}
+
+       	putchar('\n');
+
+	return (0);
+}
