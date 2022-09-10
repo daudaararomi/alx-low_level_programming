af64b86995bce09817ce0540404d8f1edb732e9c
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-10 06:53:25 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,24 +10,27 @@
 
 int main(void)
 {
-	int c1, c2, c3;
+	int c1, c2, c3, c4;
 
 	for (c1 = 0; c1 <= 9; ++c1)
 	{
 		for (c2 = 0; c2 <= 9; ++c2)
 		{
-			for (c3 = 0; c3 <= c2; c3++)
+			for (c3 = 0; c3 <= 9; c3++)
 			{
-				putchar((c1 % 10) + '0');
-				putchar((c2 % 10) + '0');
-				putchar(',');
-				putchar(' ');
-				putchar((c2 % 10) + '0');
-				putchar((c3 % 10) + '0');
-				if (c3 <= 9)
+				for (c4 =0; c4 <= 9; c4++)
 				{
+					putchar((c1 % 10) + '0');
+					putchar((c2 % 10) + '0');
 					putchar(',');
 					putchar(' ');
+					putchar((c2 % 10) + '0');
+					putchar((c3 % 10) + '0');
+					if (c1 <= 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
