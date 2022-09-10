214590824884e5b4770b2e048a31ef094c6fac52
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-09 19:01:13 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; ++c)
+	for (c = 0; c <= 9; ++c)
 	{
-		putchar(c);
+		putchar((c % 10) + '0');
 		putchar(',');
 		putchar(' ');
 	}
