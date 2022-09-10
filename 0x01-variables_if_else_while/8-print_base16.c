60805c9d970aea885eb8285f5af96ab1dda79ec9
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-09 18:27:11 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= 'f'; ++c)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
