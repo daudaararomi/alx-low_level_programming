fdcbf7fc66f12fd3c59b2a8375d66273fecded83
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-09 17:39:59 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; ++c)
+	{
+		putchar('c');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
