59bcede1d8f5e635e65b0747a168a8e64b015a1b
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-13 15:04:30 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - check the code.
+ *
+ *Return: Always 0
+ *
+ *program to print out _putchar
+ */
+int main(void)
+{
+	char name[9];
+	name = "_putchar";
+	int n;
+	for ( n = 0; n < 9; ++n)
+		putchar(name[n]);
+	putchar('\n'); /* to move to next line */
+	return (0);
+}
