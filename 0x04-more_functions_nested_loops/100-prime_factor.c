8c3ad65bde2ba38ee661b43e12edee357bc49d4d
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-15 13:48:01 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - print the largest prime factor of 612852475143
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	unsigned long int n;
+
+	n = 612852475143;
+	while ((n % 2) == 0)
+	{
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while ((n % i) == 0)
+		{
+			n /= i;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
+}
