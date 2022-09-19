4274b1bcae9a71afffc3cd56c3b0e28a7a35c49c
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-19 13:00:31 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#define "main.h"
+
+/**0-reset_to_98
+ *@m: a pointer the integer we want to set to 98
+ *
+ *Return: nothing
+ */
+void 0-reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifn def MAIN-H
+#define MAIN-H
+#include <std.io>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif /* MAIN-H
