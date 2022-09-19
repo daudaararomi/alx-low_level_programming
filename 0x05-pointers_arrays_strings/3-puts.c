e65dfd97851cd880eebac6899167ace71e748f8e
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-19 16:55:15 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts( char *str) - print a string
+ * @str : string variable
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int c = 0;
+
+	while (*(s + c) != '\0')
+	{
+		putchar(*(s + c);
+		c = c + 1;
+	}
+
+	putchar('\n');
+}
