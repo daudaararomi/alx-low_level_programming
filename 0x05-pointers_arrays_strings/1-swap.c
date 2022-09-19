2c9680b347f76a9af46ead44a8967441fd5c5b84
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-19 14:42:05 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap - swap the values of two integers
+ * @a : first integer
+ * @b : second integer
+ * @c : temporary integer
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+	c = &a;
+	a = &b;
+	b = c; 
+}
