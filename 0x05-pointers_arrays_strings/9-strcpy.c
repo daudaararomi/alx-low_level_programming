6549e82c73b7a0849beff52e0ea5baa251485c8f
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-20 02:35:03 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _srtcpy  - copy stringy
+ * _strcpy  - copy stringy
  * @dest :destination char pointer
  * @src : source char pointer
  * Return: nothing
  */
-int i = 0;
 
 char *_strcpy(char *dest, char *src)
 {
+	int i;
 	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i = i + 1;
 	}
-	dest[i] = '\0'; 
+	dest[i] = '\0';
 	return (dest);
 }
