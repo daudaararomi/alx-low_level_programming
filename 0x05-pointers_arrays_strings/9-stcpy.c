b8f89d061683409fefcac0a3069d0704bde540aa
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-20 01:01:21 -0700

    9-stcpy.c

---
+++ 0x05-pointers_arrays_strings/9-stcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _srtcpy  - copy stringy
+ * @dest :detination char pointer
+ * @src : src char pointer
+ * Return: nothing
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (*dest);
+ }
