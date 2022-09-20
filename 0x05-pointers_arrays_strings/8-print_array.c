1952f24e93deea86d0c131cf53374e18051a3aab
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-20 00:32:37 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,20 +10,17 @@
 void print_array(int *a, int n)
 {
 	int i = 0;
-
-       while (i <  n)
-       {
-	       if (i == n-1)
-	       {
+	while (i <  n)
+	{
+		if (i == n-1)
+		{
 			printf ("%d", *(a + i));
-	       }
+		}
 		else
 		{
-		printf ( "%d, ", *(a + i));
+			printf ( "%d, ", *(a + i));
 		}
 		i = i + 1;
-       	}
-
-
+	}
 	putchar('\n');
 }
