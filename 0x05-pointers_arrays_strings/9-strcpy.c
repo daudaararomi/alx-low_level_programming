5fa431b919c2868021a2e01db133aca3fd91486d
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-20 02:30:47 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,11 @@ int i = 0;
 
 char *_strcpy(char *dest, char *src)
 {
-	while (*(src + i) == '\0')
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i = i + 1;
 	}
+	dest[i] = '\0'; 
 	return (dest);
 }
