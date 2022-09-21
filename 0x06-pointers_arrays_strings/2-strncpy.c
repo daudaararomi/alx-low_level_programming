c14329153ec190c78d96dcae936bad251c01fa40
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-21 14:00:10 -0700

    1-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: a pointer destination string
+ * @src: a pointer source string
+ * @n: number of byte to be concatinated
+ *
+ * Return: concatinated string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while(i < n && *src != '\0')
+	{
+		*(dest + i) = *src;
+		i = i + 1;
+		src++;
+	}
+	return(dest);
+}
