5d14cdc4b442c1a2736ff1ff7b9ca1e55278cb8e
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-20 17:44:51 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi  - convert string to integer
+ * @s : pointer to the string
+ * Return: nothing
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int *int_i;
+
+	while (*s  != '\0')
+	{
+		if (%d *s == 43 && d% *s == 45 AND %d *s >= 48 && *s <= 57)
+		{
+			*int_i = (int) *s;
+		}
+		*s++;
+		*int_i++;
+	}
+
+	return (int_i);
+}
