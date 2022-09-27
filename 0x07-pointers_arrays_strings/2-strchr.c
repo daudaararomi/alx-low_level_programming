51f2ad73b01933c9146a77e57b7b73585fda3816
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-27 14:08:26 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a chracter in a string.
+ * @s:srtring where to locate character
+ * @c:character to locate
+ *
+ * Return: a pointer to the first occurence of the letter
+ */
+
+char *_strchr(char *s, char c)
+{
+	while ( s != NULL)
+	{
+		if ( *s == c)
+		{
+			break;
+		}
+		s = s + 1;
+	}
+      	return (s);
+}
