aba3176ece43c0de21e7cb421214ab23c94e5670
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-27 12:58:41 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _memset - function to fil a memory with constant bytes
- * @n: byte of memory area pointed by s
- * @b:constant byte
- * @s:a Pointer to memory
+ * _memcpy - function that copy memory area
+ * @n:number of byte of memory area pointed by s
+ * @src:source memory area
+ * @dest:destination memory area
  *
- * Return: a pointer to the memory area s
+ * Return: a pointer to the destination  memory area
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		*(dest + i) = *(src + i);
 	}
 
-	return (s);
+	return (dest);
 }
