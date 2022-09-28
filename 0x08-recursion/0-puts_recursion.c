7150bac6daec9917feaff2746c14fe3d87d0f423
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-28 13:39:05 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ * @s:a Pointer to a string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	printf ( "%c", *s);
+	s++;
+	if (*s == '\0')
+	{
+		printf ("\n");
+	}
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+}
