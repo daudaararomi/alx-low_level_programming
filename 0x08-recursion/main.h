abf1a09ae1c60cc8d816f0c84e246b4203430e3d
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-09-29 08:36:24 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,7 @@ void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_found(int n, int mid );
+int _sqrt_recursion(int n);
 
 #endif /* MAIN_H */
