b761fa15f6e5b2b483ff34d09482ffacdacdc108
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-02 16:46:49 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i] < 0 && atoi(argv[i] > 9)
+			 {
+				puts("Error");
+				return (1);
+			}
+			 a = a + atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
