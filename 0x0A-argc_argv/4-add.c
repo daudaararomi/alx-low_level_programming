f3b4f71b75731f921a431fa7ed966cc704e9eef0
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-02 17:24:43 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,12 +14,15 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) < 0 && atoi(argv[i]) > 9)
+		if (atoi(argv[i]) == 0 && atoi(argv[i]) <= 9)
+		{
+			a = a + atoi(argv[i]);
+		}
+		else
 		{
 			puts("Error");
 			return (1);
 		}
-		a = a + atoi(argv[i]);
 	}
 	printf("%d\n", a);
 	return (0);
