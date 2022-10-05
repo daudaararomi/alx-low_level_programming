f303dbea336b68c10707a8914652aa295c6b91ad
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-04 20:02:52 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *alloc_grid -function that returns a pointer to a 2 dimensional arrayofintegers
+ * @width: number of row
+ * @height: number of column
+ * Return: pointer to concantenated string or NULL if error
+ */
+int **alloc_grid(char *s1, char *s2)
+{
+	int **array;
+	int i, j;
+
+	if (width < 0)
+		return (NULL);
+	if (height < 0)
+		return (NULL);
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(array[j]);
+			free(array);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return (array);
+}
