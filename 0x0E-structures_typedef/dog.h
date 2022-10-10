5171e9ad24ce31b4812a9a47cc69f62b5dbbb45c
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-10 13:27:45 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,15 @@
  * @owner: owner of the dog
  */
 struct dog
-
 {
 	char *name;
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
