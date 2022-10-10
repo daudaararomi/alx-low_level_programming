3db73fe0a00a724de4f5eb7318dd27ea75a2c68d
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-10 15:37:18 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,6 +36,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(sizeof(char) * j);
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
