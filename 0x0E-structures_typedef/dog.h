66e50826a61facb38b55952e4d274eb6ccac62a2
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-10 10:14:32 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+/**
+ * struct my_struct - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
---
+++ 0x0E-structures_typedef/mai.h
@@ -0,0 +1,4 @@
+#indef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#endif/*MAIN_H*/
