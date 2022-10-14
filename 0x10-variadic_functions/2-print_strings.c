773930e46a59fc463abe712a632fc47aed97655b
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-13 17:34:32 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	while (i < n)
 	{
 		str = va_arg(ap, char *);
-		if(str)
+		if (str)
 			printf("%s", str);
 		else
 			printf("(nil)");
