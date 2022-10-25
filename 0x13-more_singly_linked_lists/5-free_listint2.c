49ac0e8ba2137baad6568b5f072d2f8a6e6981ce
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-25 05:13:03 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,16 +9,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *next;
 
-	if (head != NULL)
+	if (head == NULL)
 		return;
-	temp = *head;
-	while (*temp != NULL)
+	while (*head != NULL)
 	{
-		*head = (*head)->next;
-		free(*temp);
-		temp = temp->next
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-
 }
