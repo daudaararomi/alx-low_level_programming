1e6e6a39e265302afc3b06578c2544825a464f03
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-25 03:07:14 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint -that frees a listint_t list.
+ * @head: douuble pointer to the beginning of a listint_t list
+  *
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		freeNode(head);
+		head = next;
+	}
+}
