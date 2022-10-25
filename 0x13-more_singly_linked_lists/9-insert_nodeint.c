5ff4fabe80f14eebfc92e3c91815c7df6e0dbb5d
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-25 12:16:27 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,10 +19,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	if(idx > 0)
+	if (idx > 0)
 	{
 		current = *head;
-		while (i < idx)
+		while (i < idx - 1 && current != NULL)
 		{
 			current = current->next;
 			i++;
