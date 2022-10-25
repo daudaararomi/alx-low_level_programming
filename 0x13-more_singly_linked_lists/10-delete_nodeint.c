98b4d6cb9e01bf5ba489875a22362fde6438341e
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-25 14:38:16 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	temp = *head;
-	while ( n < index)
+	while (n < index - 1)
 	{
 		if (temp->next == NULL)
 			return (-1);
