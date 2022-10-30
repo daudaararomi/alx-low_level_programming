9a1f8a1ab7ee71192f31710a628303c85ea6200c
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-30 00:38:27 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,16 +10,7 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int bi;
-	/*unsigned int mask = 32768;
 
-	while (mask > 0)
-	{
-		if ((n & mask) == 0)
-			putchar('0');
-		else
-			putchar('1');
-		mask = mask >> 1;
-		}*/
 	if (n > 1)
 		print_binary(n >> 1);
 	bi = n & 1;
