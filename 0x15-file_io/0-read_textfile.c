f97f6d483ecf78a3861bd191875f745828d3ac79
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-10-31 21:23:21 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *@filename: file name to read
+ *@letters: number of letters to print
+ *
+ *Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, sz, i = 0;
+
+	char *c = (char *)malloc(sizeof(char) * letters);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		return (0);
+	}
+	if (filename == NULL)
+		return (0);
+	sz = read(fd, c, lettters);
+	if (sz == letters)
+		return (0);
+	c[sz] = '\0';
+	while (i < sz)
+	{
+		putchar(c[i] + '0');
+		i++;
+	}
+	putchar('\n');
+	return (sz);
+
+}
