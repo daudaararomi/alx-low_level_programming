e242021a7ff86587b01991dbe76b9654333ae215
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-08 13:16:38 -0800

    all

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 /**
  *print_dlistint-  prints all the elements of a dlistint_t list
- *@n: node todlistint_t list to print its elements
+ *@h: node todlistint_t list to print its elements
  *Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
